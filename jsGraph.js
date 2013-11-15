7640353f03bf6c99bfd629f1cd48f42ab429700b
Dan Swain <dan.t.swain@gmail.com>
2013-11-14 20:45:04 -0600

    Initial objects and tests

---
+++ jsGraph.js
@@ -0,0 +1,36 @@
+var jsGraph = (function(){
+    var addNode = function(node){
+        if(node.node_index == null) {
+            node.node_index = this.nodes.length;
+            this.nodes.push(node);
+        }
+        return this;
+    };
+    
+    var jsGraph = function() {
+        this.nodes = new Array;
+        this.edges = new Array;
+        this.addNode = addNode;
+    };
+
+    jsGraph.prototype = {
+        constructor: jsGraph
+    };
+
+    return jsGraph;
+})();
+
+jsGraph.Node = (function(){
+    var Node = function() {
+        this.x = null;
+        this.y = null;
+        this.node_index = null;
+        this.neighbors = new Array;
+    };
+
+    Node.prototype = {
+        constructor: Node
+    };
+
+    return Node;
+})();
--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,31 @@
-test("hello", function(){
-    ok(1, "TODO: Write tests!!");
+test("jsGraph Constructor", function(){
+    var graph = new jsGraph();
+
+    deepEqual(graph.constructor, jsGraph, "Creates an instance of jsGraph");
+    deepEqual(graph.nodes.constructor, Array, "Has a node array");
+    deepEqual(graph.edges.constructor, Array, "Has edges");
+});
+
+test("Node Constructor", function(){
+    var node = new jsGraph.Node();
+
+    deepEqual(node.x, null, "Has null x");
+    deepEqual(node.y, null, "Has null y");
+    deepEqual(node.node_index, null, "Has null node_index");
+    deepEqual(node.neighbors.constructor, Array, "Has a neighbors array");
+});
+
+test("jsGraph.addNode", function(){
+    var graph = new jsGraph();
+    var node = new jsGraph.Node();
+
+    var out = graph.addNode(node);
+    deepEqual(graph.nodes[graph.nodes.length - 1], node, "Adds the node at the end of the nodes list");
+    deepEqual(node.node_index, 0, "Sets the node index");
+    deepEqual(out, graph, "Returns the graph for chaining");
+
+    graph.addNode(node);
+    deepEqual(graph.nodes.length, 1, "Doesn't add a node twice");
+    deepEqual(node.node_index, 0, "Doesn't change the node index on repeat adds");
+    
 });
