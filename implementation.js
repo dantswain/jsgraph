3fcde2fe0ad5945cc2c3119c4b8650126aa37a29
Dan Swain <dan.t.swain@gmail.com>
2013-11-16 16:42:10 -0600

    Add node on click

--- implementation.js
+++ implementation.js
@@ -1,16 +1,27 @@
+function getMousePosition(obj, evt) {
+    var rect = obj.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
+
+// NOTE - making these global for debugging
+
+// kinetic stage
+var stage = new Kinetic.Stage({
+    container: 'container',
+    width: $('#container').width(),
+    height: $('#container').height()
+});
+// our graph
+var graph = new jsGraph();
+
 (function(){
     // Kinetic variables set up
-    var stage = new Kinetic.Stage({
-        container: 'container',
-        width: $('#container').width(),
-        height: $('#contianer').height()
-    });
-
     var node_layer = new Kinetic.Layer();
     stage.add(node_layer);
 
-    // our graph
-    var graph = new jsGraph();
     
     // callbacks
     $("#add-nodes").click(function() {
@@ -20,6 +31,11 @@
             $(this).text("Click to stop adding nodes");
             // add callback to stage
             $(stage.getContent()).click(function(event){
+                var pos = getMousePosition(this, event);
+                var node = new jsGraph.Node();
+                node.x = pos.x;
+                node.y = pos.y
+                graph.addNode(node);
             });
         } else {
             $(this).text("Click to add nodes");
